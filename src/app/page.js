430a645a9827d271970484b7b7468b3b35132439
Israel Santana de Morais <46788030847@>
2025-08-08 09:25:10 -0300

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -73,6 +73,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+
+          
           <Image
             aria-hidden
             src="/window.svg"
